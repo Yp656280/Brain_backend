e4e79038bcddff68d4c75d30edc2fd0b9c4937cb
Yp656280 <112876453+Yp656280@users.noreply.github.com>
2025-03-19 19:13:58 +0530

    .

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-echo "Installing Python..."
-apt-get update && apt-get install -y python3 python3-pip
+echo "Updating package lists..."
+apt-get update
+
+echo "Installing Python & Pip..."
+apt-get install -y python3 python3-pip
 
 # Ensure "python" command points to Python 3
 ln -s /usr/bin/python3 /usr/bin/python || true
 
-echo "Installing dependencies..."
+echo "Installing Python dependencies..."
 pip3 install -r requirements.txt
